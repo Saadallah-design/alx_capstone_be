3382490a1fe9587478e0e410ebfaada1cd7a6290
Saadallah-design <saadalla.publishing@gmail.com>
2026-01-05 12:27:41 +0700

    feat(core): enable media file serving in development

--- carRentalConfig/urls.py
+++ carRentalConfig/urls.py
@@ -31,3 +31,9 @@ urlpatterns = [
     # stripe 
     path('api/payments/', include('payments.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
